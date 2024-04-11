43521d6e652b044f325b8e5d7b546f3e8cb9d368
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-04-11 07:40:40 +0000

    mentes

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello")
+# with open("adatok.txt", "r", encoding="UTF-8") as file:
+#     file.readline()
+
+    # for i in file:
+    #     szelet = i.strip().split(";")
+    #     if int(szelet[-1]) > 1:
+    #         print(szelet[1])
+
+file = open("adatok.txt")
+file.readline()
+[print(szelet[1]) for i in file if (szelet := i.strip().split(";")) and int(szelet[-1]) > 1]
+file.close()
+print()
+
+file2 = open("adatok.txt")
+file2.readline()
+print(len([i for i in file2 if (szelet := i.strip().split(";")) and (int(szelet[-2]) - int(szelet[-1])) != 3 ]))
+file2.close()
+print()
+
+file3 = open("adatok.txt")
+file3.readline()
+[print(i) for i in file3 if (szelet2 := i.strip().split(";")) and szelet2[1] == "Hát Izsák"]
+file2.close()
+print()
